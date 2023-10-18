55116f5aa9e9a790ecc17d09d29ae9b7f6453b2c
Shamimayesmin <shamimayesmin144@gmail.com>
2023-10-18 20:04:54 +0600

    breaking the limits

--- src/components/UI/AllNews.js
+++ src/components/UI/AllNews.js
@@ -55,7 +55,7 @@ const AllNews = ({ allNews }) => {
 								}}
 							>
 								{" "}
-								hello
+								
 							</div>
 
 							<p
--- src/pages/index.js
+++ src/pages/index.js
@@ -38,5 +38,6 @@ export const getStaticProps = async()=>{
     props : {
       allNews : data,
     },
+    revalidate: 10,
   }
 }
